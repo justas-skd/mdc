64c6715f7f1fb9a8a7cc13cca080193c36be12ed
Justas Kalpokas <jk@gile.local>
2017-04-10 14:26:13 +0300

    add validation for prices

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ApplicationRecord
   @allowed_vat_values = [9,21]
   belongs_to :supplier
-  validates :vat, presence: true, length: { minimum: 3 } #:inclusion=> { :in => @allowed_vat_values}
-  # validates :address, presence: true,
-  #                   length: { minimum: 1 }
+  validates :vat, presence: true, :inclusion=> { :in => @allowed_vat_values}
+  validate :check_purchase_price_and_sale_price
 
+  def check_purchase_price_and_sale_price
+    errors.add(:sale_price, "can't be smaller than purchase price") if purchase_price < sale_price
+  end
 end
+  # message: :vat" is not in @{allowed_vat_values}"}
+  #length: { minimum: 3 }
